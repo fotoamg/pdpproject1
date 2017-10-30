a57b25d1b0cc90252403553241a60aec7130198c
BUDAPEST\Miklos_Aszriev <Miklos_Aszriev@epam.com>
2017-10-30 15:57:37 +0100

    webpack dev server added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+var path = require('path');
+
 module.exports = {
     entry: './assets/js/script1.js',
-    output: { filename : './public/js/bundle.js'},
+    output: {
+        path: path.resolve(__dirname, 'public/js/'),
+        filename : 'bundle.js'
+    },
     module: {
         loaders: [
             {
